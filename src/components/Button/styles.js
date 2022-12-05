9bca66a2ce01d47796bd09494e8f575446655597
eliezer Maciel <eliezeromaciel@gmail.com>
2022-12-05 19:13:59 -0300

    modifica cor texto

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const StyledButton = styled.button`
     border-radius: 30%;
     background-color:grey;
+    color: white;
     border: none;
     width: 50px;
     height: 50px;
